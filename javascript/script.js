4a82397d2b3c17216fa2b2eae3440b9d483770dd
dharaniravikumar <dharaniravikumar8353@gmail.com>
2025-06-06 16:38:25 +0530

    add dom.html

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+var arr=[10,20,30,40];
+var arr2=[50,60,70];
+console.log(arr)                 
+console.log(arr2)
+
+var marks=[90,100,89,78,99];
+var[m1,m2,m3,m4,m5]=marks;
+console.log(m1)
+console.log(m2)
+console.log(m3)
+console.log(m4)
+console.log(m5)
+
+var a=10
+var b=20
+var sum =a+b;
+console.log(a,"+",b,"=",sum)
+console.log(`${a}+${b}=${sum}`)
