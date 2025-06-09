2debd902bf7ef9fe778896640af6705cdf256680
dharaniravikumar <dharaniravikumar8353@gmail.com>
2025-06-09 16:21:07 +0530

    add task.html

---
+++ javascript/python/condition.py
@@ -0,0 +1,19 @@
+a=10
+b=20
+c=30
+if a>b and a>c:
+    print("a is greater")
+elif b>a and b>c:
+    print("b is greater")    
+else:
+    print("c is greater")    
+
+for i in range(5):
+    print(i) 
+
+for i in range(1,3):
+    print(i) 
+
+def add(a,b):
+    return a+b;
+print(add(10,20))       
---
+++ javascript/python/demo.py
@@ -0,0 +1,25 @@
+"""a=10
+b=20
+print(a+b)
+print(type(a))
+c="string"
+d=True
+pi=3.14
+print(type(c))
+print(type(d))
+print(type(pi))
+
+list=[1,2,3,4,5,6,7,8,9,10];
+for i in list:
+    print(i)"""
+
+"""tuple=(6,7,8,9,10);
+for i in tuple:
+    print(i)
+    print(type(i))"""
+
+dictionary={"name":"viky","password":"123456"}
+print(dictionary["name"])
+print(type(dictionary))
+for key in dictionary:
+    print(dictionary [key])
